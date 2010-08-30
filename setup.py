e56a97618b93ff93d9f2c6f27c96f0b9dc6408b0
Raphaël Hertzog <hertzog@debian.org>
2010-08-30 09:38:58 +0200

    Add setup script and initial Debian packaging

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import glob
+import os
+import re
+
+from DistUtilsExtra.command import *
+from DistUtilsExtra.auto import setup
+
+# look/set what version we have
+changelog = "debian/changelog"
+if os.path.exists(changelog):
+    head = open(changelog).readline()
+    match = re.compile(".*\((.*)\).*").match(head)
+    if match:
+        version = match.group(1)
+#         f = open("MsgidUrl/Version.py", "w")
+#         f.write("VERSION=\"%s\"\n" % version)
+#         f.close()
+
+#GETTEXT_NAME="apturl"
+#I18NFILES = []
+#for filepath in glob.glob("po/mo/*/LC_MESSAGES/*.mo"):
+#    lang = filepath[len("po/mo/"):]
+#    targetpath = os.path.dirname(os.path.join("share/locale",lang))
+#    I18NFILES.append((targetpath, [filepath]))
+
+setup(name='message-id-url',
+      version=version,
+      scripts=['open-msgid'],
+      data_files=[('/etc/firefox/pref/',   ["data/midurl.js"]),
+                  ('share/kde4/services/', ["data/mid.protocol"]),
+                 ],
+      )
